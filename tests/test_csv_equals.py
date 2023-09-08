be1369d403ec79329d080fb274016c4e3fc9095e
salvador <salvadorebrandolin@mi.unc.edu.ar>
2023-09-07 23:50:23 -0300

    test: New test to check that functional groups in classic UNIFAC and PSRK are equally represented for consistency
    All repeated functional groups between classic UNIFAC in PSRK are now represented un psrk_subgroups.csv. TODO: represent the remainings PSRK groups and test all.

---
+++ tests/test_csv_equals.py
@@ -0,0 +1,48 @@
+from ugropy import constants
+
+# this test check if the same subgroup appears in 2 models, both
+# xxx_subgroups.csv row are the same
+
+
+def test_unifac_psrk_equal():
+    for group in constants.unifac_subgroups.index:
+        if group in constants.psrk_subgroups.index:
+            assert (
+                constants.unifac_subgroups.loc[group, "smarts"]
+                == constants.psrk_subgroups.loc[group, "smarts"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "contribute"]
+                == constants.psrk_subgroups.loc[group, "contribute"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "composed"]
+                == constants.psrk_subgroups.loc[group, "composed"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "subgroup_number"]
+                == constants.psrk_subgroups.loc[group, "subgroup_number"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "main_group"]
+                == constants.psrk_subgroups.loc[group, "main_group"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "R"]
+                == constants.psrk_subgroups.loc[group, "R"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "Q"]
+                == constants.psrk_subgroups.loc[group, "Q"]
+            )
+
+            assert (
+                constants.unifac_subgroups.loc[group, "molecular_weight"]
+                == constants.psrk_subgroups.loc[group, "molecular_weight"]
+            )
--- ugropy/constants.py
+++ ugropy/constants.py
@@ -26,19 +26,23 @@ here = Path(__file__).parent.resolve()
 
 # UNIFAC
 with open(f"{here}/groupscsv/unifac/unifac_subgroups.csv", mode="r") as f:
-    unifac_subgroups = pd.read_csv(f, sep="|", index_col="group")
+    unifac_subgroups = pd.read_csv(f, sep="|", index_col="group", comment="?")
 
 with open(f"{here}/groupscsv/unifac/unifac_matrix.csv", mode="r") as f:
-    unifac_matrix = pd.read_csv(f, sep="|", index_col="group")
+    unifac_matrix = pd.read_csv(f, sep="|", index_col="group", comment="?")
 
 with open(f"{here}/groupscsv/unifac/ch2_hideouts.csv", mode="r") as f:
-    ch2_hideouts = pd.read_csv(f, index_col="group").index
+    ch2_hideouts = pd.read_csv(f, index_col="group", comment="?").index
 
 with open(f"{here}/groupscsv/unifac/ch_hideouts.csv", mode="r") as f:
-    ch_hideouts = pd.read_csv(f, index_col="group").index
+    ch_hideouts = pd.read_csv(f, index_col="group", comment="?").index
 
 # Problematics
 with open(f"{here}/groupscsv/problematic_structures.csv", mode="r") as f:
     problematic_structures = pd.read_csv(
         f, sep="|", index_col="smarts", comment="?"
     )
+
+# PSRK
+with open(f"{here}/groupscsv/psrk/psrk_subgroups.csv", mode="r") as f:
+    psrk_subgroups = pd.read_csv(f, sep="|", index_col="group", comment="?")
