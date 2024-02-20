b229bd8a68d199c9a6dc6b5fb4a92af0425e988c
salvadorbrandolin <salvadorebrandolin@gmail.com>
2024-02-20 13:59:31 -0300

    start changing the hidden feature

--- ugropy/fragmentation_models/fragmentation_model.py
+++ ugropy/fragmentation_models/fragmentation_model.py
@@ -15,17 +15,24 @@ import pandas as pd
 class FragmentationModel:
     """FragmentationModel class.
 
-    All ugropy FragmentationModels are an instance of this class. This class
-    can be used by the user to create their own FragmentationModels.
+    All ugropy supported models are an instance of this class. This class can
+    be used by the user to create their own FragmentationModels.
 
     Parameters
     ----------
     subgroups : pd.DataFrame
-        Model's subgroups.
+        Model's subgroups. Index: 'group' (groups names). Mandatory columns:
+        'smarts' (SMARTS representations of the group), 'contribute'
+        (dictionary as a string with the group contribution), 'composed' (y or
+        n if it is or is not a composed structure), 'molecular_weight'
+        (molecular weight of the subgroups).
     main_groups : Union[pd.DataFrame, None], optional
-        _description_, by default None
+        List of main groups, by now it is optional and does nothing, by default
+        None
     problematic_structures : Union[pd.DataFrame, None], optional
-        _description_, by default None
+        Model's problematic/ambiguous structures. Index: 'smarts' (SMARTS of
+        the problematic structure). Mandatory columns: 'contribute' (dictionary
+        as a string with the structure contribution), by default None
     ch2_hideouts : List[str], optional
         _description_, by default []
     ch_hideouts : List[str], optional
@@ -39,6 +46,7 @@ class FragmentationModel:
         problematic_structures: Union[pd.DataFrame, None] = None,
         ch2_hideouts: List[str] = [],
         ch_hideouts: List[str] = [],
+        hidding_groups: List[str] = [],
     ):
         self.subgroups = subgroups
 
@@ -65,7 +73,26 @@ class FragmentationModel:
         # Contribution matrix
         self.contribution_matrix = self._build_contrib_matrix()
 
+        # Shared hideouts
+        self.hidding_groups = hidding_groups
+        self.shared_hideouts = self._build_shared_hideouts()
+
     def _build_contrib_matrix(self) -> pd.DataFrame:
+        """Build the contribution matrix of the model.
+
+        Returns
+        -------
+        pd.DataFrame
+            The contribution matrix of the model built from the contribute
+            column of the subgroups DataFrame.
+
+        Raises
+        ------
+        json.JSONDecodeError
+            Bad contribution parsing of a group
+        TypeError
+            Bad contribution parsing of a group
+        """
         index = self.subgroups.index.to_numpy()
         matrix = np.zeros((len(index), len(index)), dtype=int)
 
@@ -82,14 +109,36 @@ class FragmentationModel:
                 contribution = json.loads(str_contribution)
             except json.JSONDecodeError:
                 raise json.JSONDecodeError(
-                    f"Band contribution parsing of the group: {group}"
+                    f"Bad contribution parsing of the group: {group}"
                 )
             except TypeError:
-                raise json.JSONDecodeError(
-                    f"Band contribution parsing of the group: {group}"
+                raise TypeError(
+                    f"Bad contribution parsing of the group: {group}"
                 )
 
             for k in contribution.keys():
                 dfm.loc[group, k] = contribution[k]
 
         return dfm
+
+    def _build_shared_hideouts(self) -> pd.DataFrame:
+        shared_hideouts = pd.DataFrame(columns=["shared_group", "hideout"])
+        shared_hideouts.set_index("shared_group", inplace=True)
+
+        if len(self.hidding_groups) == 0:
+            return shared_hideouts
+
+        subgroups = self.subgroups.index
+        contributes = self.subgroups["contribute"]
+
+        for shared in self.hidding_groups:
+            for group, contribute in zip(subgroups, contributes):
+                dict_contrib = json.loads(contribute)
+
+                if dict_contrib.get(shared, 0) < 0:
+                    new_row = pd.DataFrame([(shared, group)], columns=["shared_group", "hideout"])
+                    new_row.set_index("shared_group", inplace=True)
+
+                    shared_hideouts = pd.concat([shared_hideouts, new_row])
+
+        return shared_hideouts
--- ugropy/fragmentation_models/models.py
+++ ugropy/fragmentation_models/models.py
@@ -37,48 +37,77 @@ def _rd(file_path: str, index_col: str = None) -> pd.DataFrame:
         return pd.read_csv(f, sep="|", index_col=index_col, comment="?")
 
 
+# =============================================================================
 # LV-UNIFAC
+# =============================================================================
 _uni = f"{_csvs}/unifac"
 
+_uni_sg = _rd(f"{_uni}/unifac_subgroups.csv", "group")
+_uni_mg = _rd(f"{_uni}/unifac_maingroups.csv", "no.")
+_uni_problems = _rd(f"{_csvs}/problematic_structures.csv", "smarts")
+_uni_ch2 = _rd(f"{_uni}/ch2_hideouts.csv", "group").index.to_list()
+_uni_ch = _rd(f"{_uni}/ch_hideouts.csv", "group").index.to_list()
+
 unifac = FragmentationModel(
-    subgroups=_rd(f"{_uni}/unifac_subgroups.csv", "group"),
-    main_groups=_rd(f"{_uni}/unifac_maingroups.csv", "no."),
-    problematic_structures=_rd(
-        f"{_csvs}/problematic_structures.csv", "smarts"
-    ),
-    ch2_hideouts=_rd(f"{_uni}/ch2_hideouts.csv", "group").index.to_list(),
-    ch_hideouts=_rd(f"{_uni}/ch_hideouts.csv", "group").index.to_list(),
+    subgroups=_uni_sg,
+    main_groups=_uni_mg,
+    problematic_structures=_uni_problems,
+    ch2_hideouts=_uni_ch2,
+    ch_hideouts=_uni_ch,
+    hidding_groups=["CH", "CH2"]
 )
 
 
+# =============================================================================
 # PSRK
+# =============================================================================
 _psrk = f"{_csvs}/psrk"
 
+_psrk_sg = _rd(f"{_psrk}/psrk_subgroups.csv", "group")
+_psrk_mg = _rd(f"{_psrk}/psrk_maingroups.csv", "no.")
+_psrk_problems = _rd(f"{_csvs}/problematic_structures.csv", "smarts")
+_psrk_ch2 = _rd(f"{_psrk}/ch2_hideouts.csv", "group").index.to_list()
+_psrk_ch = _rd(f"{_psrk}/ch_hideouts.csv", "group").index.to_list()
+
+
 psrk = FragmentationModel(
-    subgroups=_rd(f"{_psrk}/psrk_subgroups.csv", "group"),
-    main_groups=_rd(f"{_psrk}/psrk_maingroups.csv", "no."),
-    problematic_structures=_rd(
-        f"{_csvs}/problematic_structures.csv", "smarts"
-    ),
-    ch2_hideouts=_rd(f"{_psrk}/ch2_hideouts.csv", "group").index.to_list(),
-    ch_hideouts=_rd(f"{_psrk}/ch_hideouts.csv", "group").index.to_list(),
+    subgroups=_psrk_sg,
+    main_groups=_psrk_mg,
+    problematic_structures=_psrk_problems,
+    ch2_hideouts=_psrk_ch2,
+    ch_hideouts=_psrk_ch,
+    hidding_groups=["CH", "CH2"]
 )
 
+# =============================================================================
 # Dortmund
+# =============================================================================
 _do = f"{_csvs}/dortmund"
 
+_do_sg =_rd(f"{_do}/dortmund_subgroups.csv", "group")
+_do_mg = _rd(f"{_do}/dortmund_maingroups.csv", "no.")
+_do_problems = _rd(f"{_do}/dortmund_problematics.csv", "smarts")
+_do_ch2 = _rd(f"{_do}/ch2_hideouts.csv", "group").index.to_list()
+_do_ch = _rd(f"{_do}/ch_hideouts.csv", "group").index.to_list()
+
 dortmund = FragmentationModel(
-    subgroups=_rd(f"{_do}/dortmund_subgroups.csv", "group"),
-    main_groups=_rd(f"{_do}/dortmund_maingroups.csv", "no."),
-    problematic_structures=_rd(f"{_do}/dortmund_problematics.csv", "smarts"),
-    ch2_hideouts=_rd(f"{_do}/ch2_hideouts.csv", "group").index.to_list(),
-    ch_hideouts=_rd(f"{_do}/ch_hideouts.csv", "group").index.to_list(),
+    subgroups=_do_sg,
+    main_groups=_do_mg,
+    problematic_structures=_do_problems,
+    ch2_hideouts=_do_ch2,
+    ch_hideouts=_do_ch,
+    hidding_groups=["CH", "CH2"]
 )
 
+# =============================================================================
 # Joback
+# =============================================================================
 _jo = f"{_csvs}/joback"
 
+_do_sg =_rd(f"{_jo}/joback_subgroups.csv", "group")
+_do_problems = _rd(f"{_jo}/joback_problematics.csv", "smarts")
+
 joback = FragmentationModel(
-    subgroups=_rd(f"{_jo}/joback_subgroups.csv", "group"),
-    problematic_structures=_rd(f"{_jo}/joback_problematics.csv", "smarts"),
+    subgroups=_do_sg,
+    problematic_structures=_do_problems,
 )
