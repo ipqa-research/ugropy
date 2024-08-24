f89909b4e4f5cca8ffb0b0b42838436312f37c9e
salvador <salvadorebrandolin@mi.unc.edu.ar>
2024-08-24 15:26:48 -0300

    fix the test that macOS doesn't like anymore

--- tests/writers/test_to_clapeyron.py
+++ tests/writers/test_to_clapeyron.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-import pandas as pd
-
 import pytest
 
 from ugropy import Groups
@@ -16,13 +14,16 @@ path_db = here / "test_expected_result"
 
 def test_to_clapeyron():
     with open(path_db / "molarmass.csv", mode="r") as f:
-        df_molarmass = pd.read_csv(f, sep="|", index_col=None)
+        df_molarmass = f.read()
 
     with open(path_db / "ogUNIFAC_groups.csv", mode="r") as f:
-        df_unifac = pd.read_csv(f, sep="|", index_col=None)
+        df_unifac = f.read()
 
     with open(path_db / "PSRK_groups.csv", mode="r") as f:
-        df_psrk = pd.read_csv(f, sep="|", index_col=None)
+        df_psrk = f.read()
+
+    with open(path_db / "critical.csv", mode="r") as f:
+        df_critical = f.read()
 
     limonene = Groups("CC1=CCC(CC1)C(=C)C", "smiles")
     ethanol = Groups("CCO", "smiles", normal_boiling_temperature=78 + 273.15)
@@ -36,43 +37,23 @@ def test_to_clapeyron():
     )
 
     with open(here / "database" / "critical.csv", mode="r") as f:
-        df_critical_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_critical_ugropy = f.read()
 
     with open(here / "database" / "molarmass.csv", mode="r") as f:
-        df_molarmass_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_molarmass_ugropy = f.read()
 
     with open(
         here / "database" / "ogUNIFAC" / "ogUNIFAC_groups.csv", mode="r"
     ) as f:
-        df_unifac_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_unifac_ugropy = f.read()
 
     with open(here / "database" / "PSRK/PSRK_groups.csv", mode="r") as f:
-        df_psrk_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_psrk_ugropy = f.read()
 
-    assert df_unifac.equals(df_unifac_ugropy)
-    assert df_psrk.equals(df_psrk_ugropy)
-    assert df_molarmass.equals(df_molarmass_ugropy)
-
-    assert (
-        df_critical_ugropy.columns.to_numpy()[0]
-        == "Clapeyron Database File,,,,,"
-    )
-    assert (
-        df_critical_ugropy.iloc[0].to_numpy()[0]
-        == "Critical Single Parameters,,,,,"
-    )
-    assert (
-        df_critical_ugropy.iloc[1].to_numpy()[0]
-        == "species,CAS,Tc,Pc,Vc,acentricfactor"
-    )
-    assert (
-        df_critical_ugropy.iloc[2].to_numpy()[0]
-        == "limonene,,657.4486692170663,2755561.066677689,0.0004965,0.3219127551737521"  # noqa
-    )
-    assert (
-        df_critical_ugropy.iloc[3].to_numpy()[0]
-        == "ethanol,,519.5440517502864,5756641.437226128,0.00016649999999999998,0.5610678012451401"  # noqa
-    )
+    assert df_molarmass == df_molarmass_ugropy
+    assert df_psrk == df_psrk_ugropy
+    assert df_unifac == df_unifac_ugropy
+    assert df_critical == df_critical_ugropy
 
     os.remove(here / "database" / "critical.csv")
     os.remove(here / "database" / "molarmass.csv")
@@ -85,13 +66,16 @@ def test_to_clapeyron():
 
 def test_to_clapeyron_batch_name():
     with open(path_db / "molarmass.csv", mode="r") as f:
-        df_molarmass = pd.read_csv(f, sep="|", index_col=None)
+        df_molarmass = f.read()
 
     with open(path_db / "ogUNIFAC_groups.csv", mode="r") as f:
-        df_unifac = pd.read_csv(f, sep="|", index_col=None)
+        df_unifac = f.read()
 
     with open(path_db / "PSRK_groups.csv", mode="r") as f:
-        df_psrk = pd.read_csv(f, sep="|", index_col=None)
+        df_psrk = f.read()
+
+    with open(path_db / "critical.csv", mode="r") as f:
+        df_critical = f.read()
 
     limonene = Groups("CC1=CCC(CC1)C(=C)C", "smiles")
     ethanol = Groups("CCO", "smiles", normal_boiling_temperature=78 + 273.15)
@@ -106,45 +90,25 @@ def test_to_clapeyron_batch_name():
     )
 
     with open(here / "database" / "otacon_critical.csv", mode="r") as f:
-        df_critical_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_critical_ugropy = f.read()
 
     with open(here / "database" / "otacon_molarmass.csv", mode="r") as f:
-        df_molarmass_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_molarmass_ugropy = f.read()
 
     with open(
         here / "database" / "ogUNIFAC" / "otacon_ogUNIFAC_groups.csv", mode="r"
     ) as f:
-        df_unifac_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_unifac_ugropy = f.read()
 
     with open(
         here / "database" / "PSRK" / "otacon_PSRK_groups.csv", mode="r"
     ) as f:
-        df_psrk_ugropy = pd.read_csv(f, sep="|", index_col=None)
+        df_psrk_ugropy = f.read()
 
-    assert df_unifac.equals(df_unifac_ugropy)
-    assert df_psrk.equals(df_psrk_ugropy)
-    assert df_molarmass.equals(df_molarmass_ugropy)
-
-    assert (
-        df_critical_ugropy.columns.to_numpy()[0]
-        == "Clapeyron Database File,,,,,"
-    )
-    assert (
-        df_critical_ugropy.iloc[0].to_numpy()[0]
-        == "Critical Single Parameters,,,,,"
-    )
-    assert (
-        df_critical_ugropy.iloc[1].to_numpy()[0]
-        == "species,CAS,Tc,Pc,Vc,acentricfactor"
-    )
-    assert (
-        df_critical_ugropy.iloc[2].to_numpy()[0]
-        == "limonene,,657.4486692170663,2755561.066677689,0.0004965,0.3219127551737521"  # noqa
-    )
-    assert (
-        df_critical_ugropy.iloc[3].to_numpy()[0]
-        == "ethanol,,519.5440517502864,5756641.437226128,0.00016649999999999998,0.5610678012451401"  # noqa
-    )
+    assert df_molarmass == df_molarmass_ugropy
+    assert df_psrk == df_psrk_ugropy
+    assert df_unifac == df_unifac_ugropy
+    assert df_critical == df_critical_ugropy
 
     os.remove(here / "database" / "otacon_critical.csv")
     os.remove(here / "database" / "otacon_molarmass.csv")
@@ -168,7 +132,7 @@ def test_molar_mass_csv():
     )
 
     with open(here / "database" / "molarmass.csv", mode="r") as f:
-        df_molarmass_unifac = pd.read_csv(f, sep="|", index_col=None)
+        df_molarmass_unifac = f.read()
 
     os.remove(here / "database" / "molarmass.csv")
 
@@ -180,20 +144,9 @@ def test_molar_mass_csv():
     )
 
     with open(here / "database" / "molarmass.csv", mode="r") as f:
-        df_molarmass_psrk = pd.read_csv(f, sep="|", index_col=None)
+        df_molarmass_psrk = f.read()
 
-    assert (
-        df_molarmass_unifac.columns.to_numpy()[0]
-        == "Clapeyron Database File,,"
-    )
-    assert (
-        df_molarmass_unifac.iloc[0].to_numpy()[0]
-        == "Molar Mases Single Params,,"
-    )
-    assert df_molarmass_unifac.iloc[1].to_numpy()[0] == "species,CAS,Mw"
-    assert df_molarmass_unifac.iloc[2].to_numpy()[0] == "limonene,,136.238"
-    assert df_molarmass_unifac.iloc[3].to_numpy()[0] == "ethanol,,46.069"
-    assert df_molarmass_unifac.equals(df_molarmass_psrk)
+    assert df_molarmass_unifac == df_molarmass_psrk
 
     os.remove(here / "database" / "molarmass.csv")
     os.remove(here / "database" / "ogUNIFAC" / "ogUNIFAC_groups.csv")
