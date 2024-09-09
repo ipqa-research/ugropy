5f3719ecb1385c3ab364bd7d80e3470005902616
Salvador <salvadorebrandolin@gmail.com>
2024-09-09 16:50:38 -0300

    keep working

---
+++ ugropy/fragmentation_models/fragmentation_gibbs_model.py
@@ -0,0 +1,55 @@
+"""GibbsModel module."""
+
+from typing import Union
+
+import pandas as pd
+
+from ugropy.fragmentation_models.fragmentation_model import FragmentationModel
+
+
+class GibbsModel(FragmentationModel):
+    """GibbsModel it's a fragmentation model dedicated to Gibbs excess models.
+
+    unifac, psrk, dortmund are instances of this class.
+
+    Parameters
+    ----------
+    subgroups : pd.DataFrame
+        Model's subgroups. Index: 'group' (subgroups names). Mandatory columns:
+        'smarts' (SMARTS representations of the group to detect its precense in
+        the molecule), 'molecular_weight' (molecular weight of the subgroups
+        used to check that the result is correct)
+    subgroups_info : Union[pd.DataFrame, None], optional
+        Information of the model's subgroups (R, Q, subgroup_number,
+        main_group), by default None
+
+    Attributes
+    ----------
+    subgroups : pd.DataFrame
+        Model's subgroups. Index: 'group' (subgroups names). Mandatory columns:
+        'smarts' (SMARTS representations of the group to detect its precense in
+        the molecule), 'molecular_weight' (molecular weight of the subgroups
+        used to check that the result is correct)
+    detection_mols : dict
+        Dictionary cotaining all the rdkit Mol object from the detection_smarts
+        subgroups column
+    subgroups_info : pd.DataFrame
+        Information of the model's subgroups. Columns: R, Q, subgroup_number,
+        main_group. Index: 'group' (subgroups names)
+    """
+
+    def __init__(
+        self,
+        subgroups: pd.DataFrame,
+        subgroups_info: Union[pd.DataFrame, None] = None,
+    ) -> None:
+        super().__init__(subgroups)
+
+        # subgroups info
+        if subgroups_info is None:
+            self.subgroups_info = pd.DataFrame(
+                [],
+                columns=["group", "subgroup_number", "main_group", "R", "Q"],
+            ).set_index("group")
+        else:
+            self.subgroups_info = subgroups_info
