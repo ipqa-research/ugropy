1334df56647b4c464b024c0b5097b2e49c5d5c56
Salvador <salvadorebrandolin@gmail.com>
2024-12-16 16:49:39 -0300

    redefinition of agani class, working on secondary structures, error on preprocess

--- ugropy/core/frag_classes/abdulelah_gani/__init__.py
+++ ugropy/core/frag_classes/abdulelah_gani/__init__.py
@@ -1,6 +1,6 @@
 """Abdulelah-Gani frag classes module."""
 
-from . import abdulelah_gani_p, abdulelah_gani_p_result
+from . import abdulelah_gani_pst, abdulelah_gani_pst_result
 
 
-__all__ = ["abdulelah_gani_p", "abdulelah_gani_p_result"]
+__all__ = ["abdulelah_gani_pst", "abdulelah_gani_pst_result"]
