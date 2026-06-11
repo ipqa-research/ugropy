f7f79ca32ea08c239c8e3142b9e88a5e60ed72b6
Salvador <salvadorebrandolin@gmail.com>
2026-06-11 13:53:30 -0300

    not_github pytest marker

--- tests/core/test_instantiate_mol_object.py
+++ tests/core/test_instantiate_mol_object.py
@@ -1,11 +1,22 @@
 import pytest
 
+from rdkit import Chem
+
 from ugropy import instantiate_mol_object
 
 
+@pytest.mark.not_github
 def test_making_it_explode():
     with pytest.raises(ValueError):
         instantiate_mol_object("snake", "mol")
 
     with pytest.raises(ValueError):
         instantiate_mol_object("acetone", "Argentina")
+
+
+@pytest.mark.not_github
+def test_instante_from_name():
+    mol_name = instantiate_mol_object("ethanol", "name")
+    mol_smiles = instantiate_mol_object("CCO", "smiles")
+
+    assert Chem.MolToSmiles(mol_name) == Chem.MolToSmiles(mol_smiles)
