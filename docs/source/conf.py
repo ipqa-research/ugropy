1c520e41327cee7822d15552ab512c08238f7dd0
SalvadorBrandolin <salvadorebrandolin@gmail.com>
2026-07-01 18:57:05 -0300

    v3.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 # EXTRA CONF
 # =============================================================================
 # nbsphinx
-nbsphinx_execute = "never"
+nbsphinx_execute = "always"
 
 autodoc_member_order = "bysource"
 
